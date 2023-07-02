0e9b9e34adefc1e80d096e1f6cf4110d52d704eb
leticiaramosmacedo <lettsramos@gmail.com>
2023-07-02 12:04:53 -0300

    correçao busca

--- src/pesquisa.js
+++ src/pesquisa.js
@@ -22,7 +22,9 @@ ready(function () { // aqui dentro vai o codigo que eu preciso
     var foo = getParameterByName('search');
     document.getElementById("input").value = foo;
 
+    console.log(foo);
 
+    /*
     // puxando os posts para criar a tela inicial 
     fetch('http://localhost:3000/posts')
         .then((response) => {
@@ -58,10 +60,71 @@ ready(function () { // aqui dentro vai o codigo que eu preciso
 
             }
         }
-        })
+        }) */
 
 
 
+        function apagarImagem(id) {
+            // Fazer uma requisição DELETE para o servidor para remover a imagem pelo ID
+            fetch("http://localhost:3000/imagens/" + id, {
+                method: "DELETE"
+            })
+                .then(function (response) {
+                    // Verificar se a requisição foi bem-sucedida
+                    if (response.ok) {
+                        // Remover a div da imagem da página HTML
+                        var div = document.getElementById("imagem-" + id);
+                        div.remove();
+                    } else {
+                        throw new Error("Erro ao apagar a imagem.");
+                    }
+                })
+                .catch(function (error) {
+                    alert(error.message);
+                });
+        } 
+        // Obtém os dados salvos no localStorage
+        var dadosArmazenados = localStorage.getItem("dados");
+        
+        // Verifica se existem dados salvos
+        if (dadosArmazenados) {
+               
+            // Converte os dados de JSON para um array
+            var dados = JSON.parse(dadosArmazenados);
+            console.log(dados);
+        
+            // Obtém a referência do elemento para exibir as imagens, textos e opções
+            var conteudo = document.getElementById("conteudo");
+        
+            // Itera sobre os dados e cria os elementos para exibir as imagens, textos e opções
+            //dados.forEach(function (objetoImagemTexto) {
+
+                const todosPosts = document.querySelector('#posts'); // criei uma constante que recebe o elemento div que irá receber todos os posts    
+
+            for (var i=0; i<dados.length; i++){ 
+           // function(objetoImagemTexto){
+                if(dados[i].texto.search(foo) != -1){
+                    console.log(dados[i].texto);
+
+                    const div_post = document.createElement('div'); // vou criar uma div para cada post 
+                todosPosts.appendChild(div_post); // fazer a div que criei para cada post ser filha da div com todos os posts 
+
+                    // adicionando legenda 
+                    const legenda = document.createElement('p');
+                    legenda.innerHTML = dados[i].texto;
+
+                    // adicionando a imagem
+                    const imagem = document.createElement('img');
+                    imagem.setAttribute('src', dados[i].urlImagem);
+
+                    // fazendo os elementos necessários serem filho da div que contem os dados de cada post 
+                    div_post.appendChild(imagem);
+                    div_post.appendChild(legenda);
+
+                }
+            };
+        }
+        
 
 });
 
